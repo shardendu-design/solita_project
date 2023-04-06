29f700beb61b468fe82b353172da829b754cce03
Shardendu Jha <shardendujha@87-92-0-224.bb.dnainternet.fi>
2023-04-06 18:55:55 +0300

    create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import create_app, db
+
+
+
+if __name__ == '__main__':
+    flask_app = create_app('dev')
+    # with flask_app.app_context():
+    #     db.create_all()
+
+    flask_app.run(host="localhost", port=8888, debug=True)
+    
