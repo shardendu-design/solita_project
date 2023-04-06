95ae1b133e24ef683434ef2b08ef1cb357db4cd7
Shardendu Jha <shardendujha@87-92-0-224.bb.dnainternet.fi>
2023-04-06 23:35:39 +0300

    port number change with 8080

--- run.py
+++ run.py
@@ -4,8 +4,7 @@ from app import create_app
 
 if __name__ == '__main__':
     flask_app = create_app('dev')
-    # with flask_app.app_context():
-    #     db.create_all()
+   
 
     flask_app.run(host="localhost", port=8080, debug=True)
     
